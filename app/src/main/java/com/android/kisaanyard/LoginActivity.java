bded003feff3cb1822caac6c76ce53ebb8f43f84
wasi-mirza <wasidevil@gmail.com>
2020-11-04 13:21:02 +0530

    login crash

--- app/src/main/java/com/android/kisaanyard/LoginActivity.java
+++ app/src/main/java/com/android/kisaanyard/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.android.kisaanyard;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +16,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Log.d("TAG", "onCreate: " + "HELLO WORLD");
+
+        showToast(this, "This is the New Changes after creating Fix");
+
+    }
+
+    @SuppressLint("ShowToast")
+    private void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT);
     }
 }
