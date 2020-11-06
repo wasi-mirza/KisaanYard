ce21d8d6facf52eac2c23ac4c37157c7f82141f5
wasi-mirza <73886897+wasi-mirza@users.noreply.github.com>
2020-11-06 16:19:06 +0530

    Revert "Made changes in ScrLoginwithOtp"

--- app/src/main/java/com/android/kisaanyard/ScrLoginWithOtp.java
+++ app/src/main/java/com/android/kisaanyard/ScrLoginWithOtp.java
@@ -11,52 +11,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.kisaanyard.Registration.MainActivityRegistration;
 import com.google.android.material.textfield.TextInputLayout;
 
-public class ScrLoginWithOtp extends AppCompatActivity implements View.OnClickListener, TextWatcher, RadioGroup.OnCheckedChangeListener {
-    private TextInputLayout til_otp, till_contact_number,till_email;
-    private EditText et_otp, et_contact_number,et_email;
+public class ScrLoginWithOtp extends AppCompatActivity implements View.OnClickListener, TextWatcher {
+    private TextInputLayout til_otp, till_contact_number;
+    private EditText et_otp, et_contact_number;
+
+
     private TextView tv_resend;
     private TextView tv_otp_timer;
-    private Button bt_continue, bt_generaateotp;
+    private TextView tv_loginpassword;
+
+    private Button bt_continue,bt_generaateotp;
     private LinearLayout ll_otp_timer;
     private LinearLayout ll_otp;
-    private LinearLayout ll_email,ll_mobile;
-    private RadioGroup radioGroup,rg_login_type;
-    private RadioButton rbt_email,rbt_mobile;
-
-    private String otp, contact_number,type,email;
-    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-
-
 
     private String otp, contact_number;
 
     private String type;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login_with_otp);
 
-
-
         Intent intent = getIntent();
         type = intent.getStringExtra("type");
 
         startTimer();
-
         initUi();
     }
+
 
     public void initUi() {
 
@@ -65,144 +55,67 @@ public class ScrLoginWithOtp extends AppCompatActivity implements View.OnClickLi
 
         til_otp = findViewById(R.id.til_otp);
         till_contact_number = findViewById(R.id.till_contact_number);
-        till_email = findViewById(R.id.till_email);
+
+        tv_loginpassword=findViewById(R.id.tv_loginpassword);
 
         et_otp = findViewById(R.id.et_otp);
         et_contact_number = findViewById(R.id.et_contact_number);
         et_contact_number.addTextChangedListener(this);
-
-        et_email=findViewById(R.id.et_email);
-        et_email.addTextChangedListener(this);
-
         tv_otp_timer = findViewById(R.id.otp_timer);
         tv_otp_timer.setOnClickListener(this);
-
-
         bt_continue = findViewById(R.id.bt_continue);
-
 
         tv_resend = findViewById(R.id.tv_resend);
 
 
         tv_resend.setOnClickListener(this);
-
-        bt_continue = findViewById(R.id.bt_continue);
         bt_continue.setOnClickListener(this);
-
-
-        bt_generaateotp = findViewById(R.id.bt_generateotp);
 
 
         bt_generaateotp=findViewById(R.id.bt_generateotp);
         tv_loginpassword.setOnClickListener(this);
-
         bt_generaateotp.setOnClickListener(this);
 
-       // radioGroup=findViewById(R.id.radiogroup);
-        bt_generaateotp.setClickable(false);
-
-        rg_login_type=findViewById(R.id.rg_login_type);
-        rg_login_type.setOnCheckedChangeListener(this);
-        rbt_email=findViewById(R.id.rbt_email);
-        rbt_mobile=findViewById(R.id.rbt_mobile);
-
-        ll_email=findViewById(R.id.ll_email);
-        ll_mobile=findViewById(R.id.ll_mobile);
     }
 
-    public void validateMobile()
-    {
-        boolean isValid = true;
-        contact_number = et_contact_number.getText().toString();
-        email=et_email.getText().toString();
-
-        if (contact_number.isEmpty()) {
-            isValid = false;
-            till_contact_number.setError("Enter Mobile Number");
-        } else if (contact_number.length() < 10) {
-            isValid = false;
-            till_contact_number.setError("Please enter Valid Mobile Number");
-        } else {
-            isValid = isValid && true;
-            till_contact_number.setError(null);
-        }
-
-        /*if(radioGroup.getCheckedRadioButtonId()==-1)
-        {
-            isValid=false;
-            Toast.makeText(ScrLoginWithOtp.this, "Please select Type", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-           isValid=isValid && true;
-           getType();
-        }*/
-
-        if (isValid)
-        {
-            ll_otp.setVisibility(View.VISIBLE);
-            bt_generaateotp.setVisibility(View.GONE);
-        }
-
-
-    }
-
-    public void validateEmail()
-    {
-        boolean isValid = true;
-        email=et_email.getText().toString();
-
-        if (email.isEmpty())
-        {
-            isValid=false;
-            till_email.setError("Enter Email Address");
-        }else if (!email.matches(emailPattern))
-        {
-            isValid=false;
-            till_email.setError("Enter Valid Email Address");
-
-        }else {
-            isValid=isValid && true;
-            till_email.setError(null);
-        }
-
-        if (isValid)
-        {
-            ll_otp.setVisibility(View.VISIBLE);
-            bt_generaateotp.setVisibility(View.GONE);
-        }
-
-
-
-    }
-
-    public void validateOtp() {
+    public void validate() {
         boolean isValid = true;
         otp = et_otp.getText().toString();
+        contact_number=et_contact_number.getText().toString();
 
-        if (otp.isEmpty()) {
-            isValid = false;
-            til_otp.setError("Enter OTP");
-        } else if (otp.length() < 4) {
-            isValid = false;
-            til_otp.setError("Please enter Valid OTP");
+        if (TextUtils.isEmpty(otp)) {
+            til_otp.setError("Enter Valid Otp");
+            return;
         } else {
-            isValid = isValid && true;
             til_otp.setError(null);
         }
 
-        if (isValid) {
-            Log.d("TAG","call check otp");
+
+
+
+        if (otp.length() < 4) {
+            til_otp.setError("Enter Valid Otp");
+            isValid = false;
+        } else {
+            til_otp.setError(null);
         }
 
+        if (TextUtils.isEmpty(contact_number) || contact_number.length() < 10) {
+            isValid = false;
+            till_contact_number.setError("Enter Mobile");
 
-    }
+        } else {
+            isValid = isValid && true;
+            till_contact_number.setError(null);
 
-    public void getType()
-    {
-        int id = radioGroup.getCheckedRadioButtonId();
-        RadioButton radioButton = findViewById(id);
-        type = radioButton.getText().toString();
+        }
+
+        if (isValid) {
+            //call_verify_login_with_otp_client();
+        } else {
+            Log.d("TAG", "validate: " + "invalidate");
+
+        }
     }
 
     public void startTimer() {
@@ -211,6 +124,7 @@ public class ScrLoginWithOtp extends AppCompatActivity implements View.OnClickLi
             public void onTick(long millisUntilFinished) {
                 tv_resend.setClickable(false);
                 tv_otp_timer.setText("" + millisUntilFinished / 1000);
+                //here you can have your logic to set text to edittext
             }
 
             public void onFinish() {
@@ -234,34 +148,23 @@ public class ScrLoginWithOtp extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void afterTextChanged(Editable editable) {
-        bt_generaateotp.setAlpha(1);
-        bt_generaateotp.setClickable(true);
 
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.bt_generateotp:
-                if (rbt_email.isChecked())
-                {
-                    validateEmail();
-                }else {
-                    validateMobile();
-                }
 
-                break;
+
+        switch (view.getId()) {
 
             case R.id.bt_continue:
-                validateOtp();
+                validate();
                 break;
 
             case R.id.tv_resend:
                 ll_otp_timer.setVisibility(View.VISIBLE);
                 startTimer();
                 break;
-
-
 
             case R.id.tv_loginpassword:
                 Intent intent=new Intent(this,ScrLoginWithPassword.class);
@@ -275,24 +178,10 @@ public class ScrLoginWithOtp extends AppCompatActivity implements View.OnClickLi
 
 
 
-
         }
     }
 
 
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-        if(checkedId==R.id.rbt_email)
-        {
-            ll_email.setVisibility(View.VISIBLE);
-            ll_mobile.setVisibility(View.GONE);
-        }
-        else if(checkedId==R.id.rbt_mobile)
-        {
-            ll_mobile.setVisibility(View.VISIBLE);
-            ll_email.setVisibility(View.GONE);
-        }
-    }
 
-}
+    }
 
--- app/src/main/java/com/android/kisaanyard/ScrLoginWithPassword.java
+++ app/src/main/java/com/android/kisaanyard/ScrLoginWithPassword.java
@@ -3,39 +3,25 @@ package com.android.kisaanyard;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
 
-public class ScrLoginWithPassword extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+public class ScrLoginWithPassword extends AppCompatActivity implements View.OnClickListener {
 
 
-    private EditText et_contact_number, et_password, et_email;
+    private EditText et_contact_number,et_password;
     private TextView tv_forgotpassword;
-    private TextInputLayout till_contact_number, till_password,till_email;
+    private TextInputLayout till_contact_number,till_password;
     private Button bt_login;
-
-
-    private String password, contact_number, email;
-    private RadioGroup rg_login_type;
-    private RadioButton rbt_email,rbt_mobile;
-    private LinearLayout ll_email,ll_mobile;
-
-    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     private String password, contact_number;
     private String type;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,18 +37,6 @@ public class ScrLoginWithPassword extends AppCompatActivity implements View.OnCl
 
     }
 
-
-    public void initUi() {
-        et_contact_number = findViewById(R.id.et_contact_number);
-        et_password = findViewById(R.id.et_password);
-        et_email = findViewById(R.id.et_email);
-        tv_forgotpassword = findViewById(R.id.tv_forgotpassword);
-        till_password = findViewById(R.id.til_password);
-        till_contact_number = findViewById(R.id.till_contact_number);
-        till_email = findViewById(R.id.till_email);
-
-        bt_login = findViewById(R.id.bt_login);
-
     public void initUi(){
 
         et_contact_number=findViewById(R.id.et_contact_number);
@@ -71,105 +45,49 @@ public class ScrLoginWithPassword extends AppCompatActivity implements View.OnCl
         till_password=findViewById(R.id.til_password);
         till_contact_number=findViewById(R.id.till_contact_number);
         bt_login=findViewById(R.id.bt_login);
-
         bt_login.setOnClickListener(this);
-
-        rg_login_type=findViewById(R.id.rg_login_type);
-        rg_login_type.setOnCheckedChangeListener(this);
-        rbt_email=findViewById(R.id.rbt_email);
-        rbt_mobile=findViewById(R.id.rbt_mobile);
-
-        ll_email=findViewById(R.id.ll_email);
-        ll_mobile=findViewById(R.id.ll_mobile);
-
     }
-
-
-    public void validate() {
 
     public void validate(){
 
-
         boolean isValid = true;
-        contact_number = et_contact_number.getText().toString();
-        email=et_email.getText().toString();
-        password = et_password.getText().toString();
+        contact_number=et_contact_number.getText().toString();
+        password=et_password.getText().toString();
 
-        if (rbt_mobile.isChecked())
-        {
-            if (contact_number.isEmpty()) {
-                isValid = false;
-                till_contact_number.setError("Enter Mobile");
+        if (TextUtils.isEmpty(contact_number) || contact_number.length() < 10) {
+            isValid = false;
+            till_contact_number.setError("Enter Mobile");
 
-            }else if (contact_number.length()<10){
-                isValid = false;
-                till_contact_number.setError("Enter Valid Mobile Number");
-            }else {
-                isValid = isValid && true;
-                till_contact_number.setError(null);
-            }
-
-        }else {
-            if (email.isEmpty())
-            {
-                isValid=false;
-                till_email.setError("Enter Email Address");
-            }else if (!email.matches(emailPattern))
-            {
-                isValid=false;
-                till_email.setError("Enter Valid Email Address");
-
-            }else {
-                isValid=isValid && true;
-                till_email.setError(null);
-            }
+        } else {
+            isValid = isValid && true;
+            till_contact_number.setError(null);
 
         }
-        
-        if (password.isEmpty()) {
-            isValid = false;
-            till_password.setError("Enter Password");
-        } else {
 
-            isValid = isValid && true;
+        if(password.isEmpty()){
+            isValid=false;
+            till_password.setError("Enter Password");
+        }else {
+
+            isValid=isValid && true;
             till_password.setError(null);
         }
 
-        if (isValid) {
-            Log.d("TAG", "call login Api");
-            Toast.makeText(ScrLoginWithPassword.this,"Valid",Toast.LENGTH_LONG).show();
-        }
 
     }
 
-
-
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
+        switch (view.getId()){
             case R.id.tv_forgotpassword:
-                Intent intent = new Intent(this, ScrForgotPassword.class);
+                Intent intent =new Intent(this,ScrForgotPassword.class);
                 startActivity(intent);
                 break;
 
 
             case R.id.bt_login:
-                validate();
+                    validate();
                 break;
-        }
-    }
-
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-        if(checkedId==R.id.rbt_email)
-        {
-            ll_email.setVisibility(View.VISIBLE);
-            ll_mobile.setVisibility(View.GONE);
-        }
-        else if(checkedId==R.id.rbt_mobile)
-        {
-            ll_mobile.setVisibility(View.VISIBLE);
-            ll_email.setVisibility(View.GONE);
         }
     }
 }
